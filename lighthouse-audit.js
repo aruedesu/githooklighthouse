bb394e422d41973105e5d801ab2c445d0c237d42
Geo Najarro <GNajarro@asi-dev4.com>
2018-10-01 13:03:02 +0800

    Thunder

--- lighthouse-audit.js
+++ lighthouse-audit.js
@@ -2,7 +2,7 @@ var csv = require('node-csv').createParser();
 var child = require('child_process');
 var urls = "";
 
-csv.parseFile('./url.csv', function(err, data) {
+csv.parseFile('./url_list.csv', function(err, data) {
     for (i = 0; i < data.length; i++) {
         if(i == data.length - 1){
             urls += data[i];
