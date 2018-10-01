367cedf05f72c2feb98d22798f020ed9539f28e5
Geo Najarro <GNajarro@asi-dev4.com>
2018-10-01 13:54:03 +0800

    hel

--- lighthouse-audit.js
+++ lighthouse-audit.js
@@ -1,14 +1,18 @@
 var csv = require('node-csv').createParser();
 var child = require('child_process');
-var urls = "";
+var urls = '';
+var cgf = require("committed-git-files");
+cgf(function(err, results){
+    console.log(results);
+});
 
-csv.parseFile('./url_list.csv', function(err, data) {
-    for (i = 0; i < data.length; i++) {
-        if(i == data.length - 1){
-            urls += data[i];
-        } else {
-            urls += data[i]+",";
-        }
-    }
-    child.execSync('lighthouse-batch --html -s ' + urls + ' -g', {stdio:[0,1,2]});
-}); 
+// csv.parseFile('./url_list.csv', function(err, data) {
+//     for (i = 0; i < data.length; i++) {
+//         if(i == data.length - 1){
+//             urls += data[i];
+//         } else {
+//             urls += data[i] + ',';
+//         }
+//     }
+//     child.execSync('lighthouse-batch --html -s ' + urls + ' -g', {stdio:[0,1,2]});
+// }); 
